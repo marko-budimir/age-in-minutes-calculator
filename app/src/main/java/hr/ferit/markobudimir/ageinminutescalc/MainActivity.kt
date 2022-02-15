3d6611942b617456d8d710ffa2be3dc4b756f44d
Marko Budimir <marko.budimir12@gmail.com>
2022-02-15 15:43:49 +0100

    DatePicker added

--- app/src/main/java/hr/ferit/markobudimir/ageinminutescalc/MainActivity.kt
+++ app/src/main/java/hr/ferit/markobudimir/ageinminutescalc/MainActivity.kt
@@ -1,11 +1,33 @@
 package hr.ferit.markobudimir.ageinminutescalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker(){
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, day ->
+            Toast.makeText(this, "btnDatePciker pressed", Toast.LENGTH_LONG).show()
+        }, year, month, day ).show()
+
     }
 }
